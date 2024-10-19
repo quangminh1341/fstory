5f4c057f907fae0bbe247043d9d444e6b6b0670e
Z A I I K Y <156815062+quangminh1341@users.noreply.github.com>
2024-10-19 17:17:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Xem /help"];
+const statusMessages = ["Đang xem /help"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
